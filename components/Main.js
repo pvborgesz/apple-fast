6b59a95a8bf57811be8e7a3a59ac29c155fd60d4
Paulo Victor Borges <pvbo.lima@discente.ufma.br>
2023-03-08 19:58:13 -0300

    add: editing pages

--- components/Main.js
+++ components/Main.js
@@ -1,102 +1,106 @@
 export default function Main() {
   return (
-    <section class="text-gray-600 body-font">
-      <div class="max-w-5xl pt-52 pb-24 mx-auto">
-        <h1 class="text-80 text-center font-4 lh-6 ld-04 font-bold text-white mb-6">
-          Create your next website fast and easy
+    <section class='text-gray-600 body-font'>
+      <div class='max-w-5xl pt-52 pb-24 mx-auto'>
+        <h1 class='text-80 text-center font-4 lh-6 ld-04 font-bold text-white mb-6'>
+          Realize seu sonho de ter um iPhone
         </h1>
-        <h2 class="text-2xl font-4 font-semibold lh-6 ld-04 pb-11 text-gray-700 text-center">
-          nine4 is a free to use template website for websites made
+        <h2 class='text-2xl font-4 font-semibold lh-6 ld-04 pb-11 text-gray-700 text-center'>
+          Apple Fast é o melhor lugar para comprar seus produtos Apple
           <br />
-          with Next.js and styled with Tailwind CSS
+          com o melhor preço e a melhor qualidade.
         </h2>
-        <div className="ml-6 text-center">
+        <div className='ml-6 text-center'>
           <a
-            className="inline-flex items-center py-3 font-semibold text-black transition duration-500 ease-in-out transform bg-transparent bg-white px-7 text-md md:mt-0 hover:text-black hover:bg-white focus:shadow-outline"
-            href="/"
+            className='inline-flex items-center py-3 font-semibold text-black transition duration-500 ease-in-out transform bg-transparent bg-white px-7 text-md md:mt-0 hover:text-black hover:bg-white focus:shadow-outline'
+            href='/'
           >
-            <div className="flex text-lg">
-              <span className="justify-center">View All Templates</span>
+            <div className='flex text-lg'>
+              <span className='justify-center'>Conheça Nosso Trabalho</span>
             </div>
           </a>
           <a
-            className="inline-flex items-center py-3 font-semibold tracking-tighter text-white transition duration-500 ease-in-out transform bg-transparent ml-11 bg-gradient-to-r from-blue-500 to-blue-800 px-14 text-md md:mt-0 focus:shadow-outline"
-            href="/"
+            className='inline-flex items-center py-3 font-semibold tracking-tighter text-white transition duration-500 ease-in-out transform bg-transparent ml-11 bg-gradient-to-r from-blue-500 to-blue-800 px-14 text-md md:mt-0 focus:shadow-outline'
+            href='/'
           >
-            <div className="flex text-lg">
-              <span className="justify-center">Purchase</span>
+            <div className='flex text-lg'>
+              <span className='justify-center'>Compre Conosco</span>
             </div>
           </a>
         </div>
       </div>
-      <div className="container flex flex-col items-center justify-center mx-auto">
+      <div className='container flex flex-col items-center justify-center mx-auto'>
         <img
-          className="object-cover object-center w-3/4 mb-10 border shadow-md g327"
-          alt="Placeholder Image"
-          src="./images/placeholder.png"
+          className='object-cover object-center w-3/4 mb-10 '
+          alt='Placeholder Image'
+          src='./images/placeholder.webp'
         ></img>
       </div>
-      <h2 className="pt-40 mb-1 text-2xl font-semibold tracking-tighter text-center text-gray-200 lg:text-7xl md:text-6xl">
-        Clean and tidy code.
+      <h2 className='pt-40 mb-1 text-2xl font-semibold tracking-tighter text-center text-gray-200 lg:text-7xl md:text-6xl'>
+        Conheça Nossa Trabalho
       </h2>
       <br></br>
-      <p className="mx-auto text-xl text-center text-gray-300 font-normal leading-relaxed fs521 lg:w-2/3">
-        Here is our collection of free to use templates made with Next.js &
-        styled with Tailwind CSS.
+      <p className='mx-auto text-xl text-center text-gray-300 font-normal leading-relaxed fs521 lg:w-2/3'>
+        Aqui está uma pequena amostra do que podemos fazer para você.
       </p>
-      <div className="pt-12 pb-24 max-w-4xl mx-auto fsac4 md:px-1 px-3">
-        <div class="ktq4">
-          <img className="w-10" src="https://nine4.app/favicon.png"></img>
-          <h3 class="pt-3 font-semibold text-lg text-white">
-            Lorem ipsum dolor sit amet
-          </h3>
-          <p class="pt-2 value-text text-md text-gray-200 fkrr1">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
-            tincidunt a libero in finibus. Maecenas a nisl vitae ante rutrum
-            porttitor.
+      <div className='pt-12 pb-24 max-w-4xl mx-auto fsac4 md:px-1 px-3'>
+        <div class='ktq4'>
+          <img
+            className='w-10'
+            src='./images/applefastlogo.png'
+          ></img>
+          <h3 class='pt-3 font-semibold text-lg text-white'>Consultoria</h3>
+          <p class='pt-2 value-text text-md text-gray-200 fkrr1'>
+            Esclarecer suas dúvidas, entender suas necessidades e oferecer a
+            melhor solução para o seu problema.
           </p>
         </div>
-        <div class="ktq4">
-          <img className="w-10" src="https://nine4.app/favicon.png"></img>
-          <h3 class="pt-3 font-semibold text-lg text-white">
-            Lorem ipsum dolor sit amet
-          </h3>
-          <p class="pt-2 value-text text-md text-gray-200 fkrr1">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
-            tincidunt a libero in finibus. Maecenas a nisl vitae ante rutrum
-            porttitor.
+        <div class='ktq4'>
+          <img
+            className='w-10'
+            src='./images/applefastlogo.png'
+          ></img>
+          <h3 class='pt-3 font-semibold text-lg text-white'>Pagamento</h3>
+          <p class='pt-2 value-text text-md text-gray-200 fkrr1'>
+            Nós oferecemos uma variedade de opções de pagamento para você
+            escolher e estamos sempre abertos à negociações.
           </p>
         </div>
-        <div class="ktq4">
-          <img className="w-10" src="https://nine4.app/favicon.png"></img>
-          <h3 class="pt-3 font-semibold text-lg text-white">
-            Lorem ipsum dolor sit amet
+        <div class='ktq4'>
+          <img
+            className='w-10'
+            src='./images/applefastlogo.png'
+          ></img>
+          <h3 class='pt-3 font-semibold text-lg text-white'>
+            Entrega e Instalação
           </h3>
-          <p class="pt-2 value-text text-md text-gray-200 fkrr1">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
-            tincidunt a libero in finibus. Maecenas a nisl vitae ante rutrum
-            porttitor.
+          <p class='pt-2 value-text text-md text-gray-200 fkrr1'>
+            Nós entregamos e instalamos seus produtos Apple em qualquer lugar do
+            Brasil.
           </p>
         </div>
-        <div class="ktq4">
-          <img className="w-10" src="https://nine4.app/favicon.png"></img>
-          <h3 class="pt-3 font-semibold text-lg text-white">
-            Lorem ipsum dolor sit amet
+        <div class='ktq4'>
+          <img
+            className='w-10'
+            src='./images/applefastlogo.png'
+          ></img>
+          <h3 class='pt-3 font-semibold text-lg text-white'>
+            Suporte e Garantia
           </h3>
-          <p class="pt-2 value-text text-md text-gray-200 fkrr1">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
-            tincidunt a libero in finibus. Maecenas a nisl vitae ante rutrum
-            porttitor.
+          <p class='pt-2 value-text text-md text-gray-200 fkrr1'>
+            Nós oferecemos suporte e garantia para todos os nossos produtos.
+            Todos os nossos produtos são lacrados e originais, possuindo
+            garantia de 1 ano da Apple.
           </p>
         </div>
       </div>
-      <div className="pt-32 pb-32 max-w-6xl mx-auto fsac4 md:px-1 px-3">
-        <div class="ktq4">
-          <img src="https://nine4.app/images/nine4-3.png"></img>
-          <h3 class="pt-3 font-semibold text-lg text-white">
+      <div className='pt-32 pb-32 max-w-6xl mx-auto fsac4 md:px-1 px-3'>
+        <div class='ktq4'>
+          <img src='https://nine4.app/images/nine4-3.png'></img>
+          <h3 class='pt-3 font-semibold text-lg text-white'>
             Lorem ipsum dolor sit amet
           </h3>
-          <p class="pt-2 value-text text-md text-gray-200 fkrr1">
+          <p class='pt-2 value-text text-md text-gray-200 fkrr1'>
             Fusce pharetra ligula mauris, quis faucibus lectus elementum vel.
             Nullam vehicula, libero at euismod tristique, neque ligula faucibus
             urna, quis ultricies massa enim in nunc. Vivamus ultricies, quam ut
@@ -104,12 +108,12 @@ export default function Main() {
             odio.
           </p>
         </div>
-        <div class="ktq4">
-          <img src="https://nine4.app/images/nine4-3.png"></img>
-          <h3 class="pt-3 font-semibold text-lg text-white">
+        <div class='ktq4'>
+          <img src='https://nine4.app/images/nine4-3.png'></img>
+          <h3 class='pt-3 font-semibold text-lg text-white'>
             Lorem ipsum dolor sit amet
           </h3>
-          <p class="pt-2 value-text text-md text-gray-200 fkrr1">
+          <p class='pt-2 value-text text-md text-gray-200 fkrr1'>
             Fusce pharetra ligula mauris, quis faucibus lectus elementum vel.
             Nullam vehicula, libero at euismod tristique, neque ligula faucibus
             urna, quis ultricies massa enim in nunc. Vivamus ultricies, quam ut
@@ -118,27 +122,27 @@ export default function Main() {
           </p>
         </div>
       </div>
-      <section class="relative pb-24">
-        <div class="max-w-6xl mx-auto px-4 sm:px-6 text-center">
-          <div class="py-24 md:py-36">
-            <h1 class="mb-5 text-6xl font-bold text-white">
-              Subscribe to our newsletter
+      <section class='relative pb-24'>
+        <div class='max-w-6xl mx-auto px-4 sm:px-6 text-center'>
+          <div class='py-24 md:py-36'>
+            <h1 class='mb-5 text-6xl font-bold text-white'>
+              Se inscreva para receber nossas novidades
             </h1>
-            <h1 class="mb-9 text-2xl font-semibold text-gray-200">
-              Enter your email address and get our newsletters straight away.
+            <h1 class='mb-9 text-2xl font-semibold text-gray-200'>
+              Coloque seu email para receber nossas novidades
             </h1>
             <input
-              type="email"
-              placeholder="jack@example.com"
-              name="email"
-              autocomplete="email"
-              class="border border-gray-600 w-1/4 pr-2 pl-2 py-3 mt-2 rounded-md text-gray-800 font-semibold hover:border-gray-700 bg-black"
-            />{" "}
+              type='email'
+              placeholder='email@example.com'
+              name='email'
+              autocomplete='email'
+              class='border border-gray-600 w-1/4 pr-2 pl-2 py-3 mt-2 rounded-md text-gray-800 font-semibold hover:border-gray-700 bg-black'
+            />{' '}
             <a
-              class="inline-flex items-center px-14 py-3 mt-2 ml-2 font-medium text-black transition duration-500 ease-in-out transform bg-transparent border rounded-lg bg-white"
-              href="/"
+              class='inline-flex items-center px-14 py-3 mt-2 ml-2 font-medium text-black transition duration-500 ease-in-out transform bg-transparent border rounded-lg bg-white'
+              href='/'
             >
-              <span class="justify-center">Subscribe</span>
+              <span class='justify-center'>Enviar</span>
             </a>
           </div>
         </div>
--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,26 @@
-import Head from "next/head";
-import Header from "../components/Header";
-import Main from "../components/Main";
-import Footer from "../components/Footer";
-import { NextSeo } from "next-seo";
+import Head from 'next/head';
+import Header from '../components/Header';
+import Main from '../components/Main';
+import Footer from '../components/Footer';
+import { NextSeo } from 'next-seo';
 
 export default function Home() {
   return (
-    <div className="text-black bg-black">
+    <div className='text-black bg-black'>
       <NextSeo
-        title="Home: nine4"
-        description="Welcome to nine4 homepage."
-        canonical="https://nine4-3.vercel.app/"
+        title='Home: nine4'
+        description='Welcome to nine4 homepage.'
+        canonical='https://nine4-3.vercel.app/'
         openGraph={{
-          url: "https://nine4-3.vercel.app/",
+          url: 'https://nine4-3.vercel.app/',
         }}
       />
       <Head>
-        <title>nine4</title>
-        <link rel="icon" href="/favicon.png" />
+        <title>Apple Fast</title>
+        <link
+          rel='icon'
+          href='/favicon.png'
+        />
       </Head>
       <Header />
       <Main />
